fa80dcd465bdf0c88a895c4588fb69de64a1100c
Xiaoran Qi <117570352+Comrade713@users.noreply.github.com>
2024-09-29 22:40:21 -0400

    Changing

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+
+@app.route('/time')
+def time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
 
 
 app.run(host='0.0.0.0',
